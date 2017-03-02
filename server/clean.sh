daa33f899438cc95b4d71564e87f66a1ac4ac9a2
Stepan Okhorzin <stepan.okhorzin@gmail.com>
2017-03-02 16:54:22 +0500

    GIS-14 Развернуть и сконфигурировать фреймворк Yii

--- server/clean.sh
+++ server/clean.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+docker-compose run php-composer clear-cache
+rm ./.tmp/composerinit
+
 ./stop.sh
+
 docker rm -f $(docker ps -f "status=exited" -q) 2>/dev/null; docker rmi $(docker images -q -f dangling=true) 2>/dev/null; docker volume rm $(docker volume ls -q -f dangling=true | grep -v '\-data') 2>/dev/null;
+
 exit 0
